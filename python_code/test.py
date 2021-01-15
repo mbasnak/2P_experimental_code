c4e6657e432a780c22e58dc4228e17cb86a000c3
tatsuookubo <tatsuo.okubo@gmail.com>
2021-01-15 16:37:12 -0500

    now can run python scripts from MATLAB

--- python_code/test.py
+++ python_code/test.py
@@ -1,7 +1,14 @@
+"""
+simple test for running python from MATLAB 
+Tatsuo Okubo
+2021/01/15
+"""
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('C:/src/experimental_code/python_code'))
 from socket_client_wind_debug import SocketClient  # note: different file for TO's experiments
+
 
 def experiment_code(experiment=None, time=None, logfile=None):
     experiment_param = {
@@ -23,8 +30,8 @@ if __name__ == '__main__':
     2: experiment time,
     3: logfile
     """
+
     print(sys.argv)
-    print('Running the experiment...')
     if len(sys.argv) > 1:    #from the list of arguments given to the system by the matlab code run_trial
         experiment = sys.argv[1] #make the first argument be the experiment...
         time = float(sys.argv[2]) #...etc
@@ -32,5 +39,3 @@ if __name__ == '__main__':
         experiment_code(experiment, time, logfile)
     else:  # no command line argument provided
         experiment_code()
-
-
