f3f6c2232a2a41fd9a356c0ac31c84a77f9a6672
tatsuookubo <tatsuo.okubo@gmail.com>
2021-01-26 10:16:00 -0500

    removing the _debug suffix

--- python_code/socket_client_wind.py
+++ python_code/socket_client_wind.py
@@ -31,12 +31,19 @@ class SocketClient(object):
         self.time_start = time.time()  # get the current time and use it as a ref for elapsed time
 
 
-        # Set up Phidget channels
+        # Set up Phidget channels (0-index)
+        self.aout_channel_motor = 0  # for sending the motor position to the NI-DAQ thorugh Phidget
         self.aout_channel_x = 1
         self.aout_channel_yaw = 2
-        self.aout_channel_y = 3
+        self.aout_channel_y = 3     
         self.aout_max_volt = 10.0
         self.aout_min_volt = 0.0
+
+        # Setup analog output motor
+        self.aout_motor = VoltageOutput()
+        self.aout_motor.setChannel(self.aout_channel_motor)
+        self.aout_motor.openWaitForAttachment(5000)
+        self.aout_motor.setVoltage(0.0)
 
         # Setup analog output X
         self.aout_x = VoltageOutput()
@@ -67,11 +74,14 @@ class SocketClient(object):
         self.baudrate = 115200  # 9600
         self.serialTimeout = 0.001 # blocking timeout for readline()
 
-        # specify the file for logging the data from Arduino
+        # log for Arduino
         self.file_path = "C:\\Users\\Tots\\Documents\\Python\\CLwind\\log\\"
         now = datetime.datetime.now()
         date_str = now.strftime("%Y%m%d_%H%M%S")
-        self.file_name = self.file_path + date_str + ".csv"
+        self.file_name_arduino = self.file_path + date_str + "_arduino.csv"
+
+        # log for FicTrac
+        self.file_name_fictrac = self.file_path + date_str + "_fictrac.csv"
 
         # flag for indicating when the trial is done
         self.done = False
@@ -92,7 +102,8 @@ class SocketClient(object):
         # open file for logging Arduino outputs
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock,\
          serial.Serial(self.COM, self.baudrate, timeout=self.serialTimeout) as ser, \
-         open(self.file_name, mode='w') as f:
+         open(self.file_name_arduino, mode='w') as f_arduino, \
+         open(self.file_name_fictrac, mode='w') as f_fictrac:
             sock.bind((self.HOST, self.PORT))  # takes one argument, give it as a tuple
             sock.setblocking(False)  # make it non-blocking
     
@@ -100,22 +111,21 @@ class SocketClient(object):
             data = ""
             timeout_in_seconds = 1
 
-            while not self.done:
+            while not self.done:  # main loop
 
-                msg = ser.readline()
-                motor_pos = msg[:-2]  # remove \r and \n
-                print(str(animal_heading_360) + ", " + str(motor_pos, 'utf-8'))
-
-                # write to Arduino log here
-                
-                # display most recent message from Arduino if available
-                #msg = ser.readline()
-                #motor_pos = msg[:-2]  # remove \r and \n
-                #while len(msg) > 0:  # keep on reading until the message form Arduino is empty
-                #    motor_pos = msg[:-2]  # remove \r and \n
-                #    msg = ser.readline()
-
-                #print("message from Arduino: ", msg)
+                # listening to Arduino
+                msg = ser.readline()  # read from serial until there's a \n
+                time_now = time.time() 
+                if msg:  # message is not empty
+                    try:
+                        arduino_line = msg.decode('utf-8')[:-2]  # decode and remove \r and \n
+                        motor_info = arduino_line.split(", ")
+                        log_arduino = str("{:.7f}".format(time_now)) + "," + motor_info[0] + "\n"  # first element is the current motor position (0-360 deg)
+                        #log_arduino = str("{:.7f}".format(time_now)) + "," + motor_info[0].strip() + "," + motor_info[1].strip() + "," + motor_info[2].strip() + "\n"  # if the Arduino is outputting 3 vals
+                        f_arduino.writelines(log_arduino)
+                        #print("worked:", log_arduino)
+                    except:
+                        print("failed:", str("{:.7f}".format(time_now)), msg)
                 
                 # ask the OS whether the socket is readable
                 # give 3 lists of sockets for reading, writing, and checking for errors; we only care about the first one
@@ -165,30 +175,34 @@ class SocketClient(object):
                     arduino_str = "H " + str(animal_heading_360) + "\n"  # "H is a command used in the Arduino code to indicate heading
                     arduino_byte = arduino_str.encode()  # convert unicode string to byte string
                     ser.write(arduino_byte)  # send to serial port  
-                    
-                    # receive motor position from Arduino
-                    #msg = ser.readline()
-                    #motor_pos = msg[:-2]  # remove \r and \n
-                    #print(str(animal_heading_360) + ", " + str(motor_pos, 'utf-8'))
 
-                    # write Arduino ouptut to log file
-                    log = str(self.time_elapsed) + "," + str(self.frame) + "," + str(animal_heading_360) + "," + str(motor_pos, 'utf-8') + "\n"
-                    f.writelines(log)
+                    # write to FicTrac log file
+                    time_now = time.time()
+                    log_fictrac = str("{:.7f}".format(time_now)) + "," + str(animal_heading_360) + "\n"
+                    f_fictrac.writelines(log_fictrac)
 
-                    #Set Phidget voltages using FicTrac data
+                    ### Set Phidget voltages using FicTrac data
+                    # Set analog output voltage motor ()
+                    if 'motor_info' in locals():  # message from Arduion found
+                        try:
+                            motor_pos = (int(motor_info[0]) / 360) * 2 * np.pi  # convert motor position from deg to rad
+                            output_voltage_motor = motor_pos * (self.aout_max_volt-self.aout_min_volt) / (2 * np.pi)
+                            self.aout_motor.setVoltage(output_voltage_motor)
+                        except:  # the message from Arduino might not be a int value
+                            pass
+
                     # Set analog output voltage X
-                    wrapped_intx = (self.intx % (2 * np.pi))
+                    wrapped_intx = (self.intx % (2 * np.pi))                   
                     output_voltage_x = wrapped_intx * (self.aout_max_volt - self.aout_min_volt) / (2 * np.pi)
                     self.aout_x.setVoltage(output_voltage_x)
 
                     # Set analog output voltage YAW
-                    output_voltage_yaw = (self.heading)*(self.aout_max_volt-self.aout_min_volt)/(2 * np.pi)
+                    output_voltage_yaw = (self.heading) * (self.aout_max_volt-self.aout_min_volt) / (2 * np.pi)
                     self.aout_yaw.setVoltage(output_voltage_yaw) 
 
                     # Set analog output voltage Y
                     wrapped_inty = self.inty % (2 * np.pi)
-                    output_voltage_y = wrapped_inty * (self.aout_max_volt - self.aout_min_volt) / (
-                                2 * np.pi)
+                    output_voltage_y = wrapped_inty * (self.aout_max_volt - self.aout_min_volt) / (2 * np.pi)
                     self.aout_y.setVoltage(output_voltage_y)
 
                     # Save data in log file
@@ -197,14 +211,14 @@ class SocketClient(object):
                     # Display status message
                     if self.print:
                         #print('frame:  {0}'.format(self.frame))
-                        #print('time elapsed:   {0:1.3f}'.format(self.time_elapsed))
+                        print('time elapsed:   {0:1.3f}'.format(self.time_elapsed))
                         #print('yaw:   {0:1.3f}'.format(animal_heading_360))                  
                         #print('volt:   {0:1.3f}'.format(output_voltage_yaw))
                         #print('int x:   {0:1.3f}'.format(wrapped_intx))
                         #print('volt:   {0:1.3f}'.format(output_voltage_x))
                         #print('int y:   {0:1.3f}'.format(wrapped_inty))
                         #print('volt:   {0:1.3f}'.format(output_voltage_y))
-                        print()
+                        #print()
 
                     if self.time_elapsed > self.experiment_time:
                         self.done = True
