bdd2b1f30d185fc241f4d4b0f111cedb8863c53e
Tatsuo Okubo <tatsuo.okubo@gmail.com>
2021-03-01 14:00:55 -0500

    fixed a variable name in process_data.m
    fixed variable names (data.Intx -> data.ficTrac.Intx) to make it
    compatible with singleTrialVelocityAnalysis9mm

--- matlab_code/analysis_utils/process_data.m
+++ matlab_code/analysis_utils/process_data.m
@@ -16,9 +16,9 @@ settings.panels_x_DAQ_AI = 5;
 settings.panels_y_DAQ_AI = 6;
 
 
-data.Intx = trial_data( :, settings.fictrac_x_DAQ_AI ); %data from x channel
-data.angularPosition = trial_data( :, settings.fictrac_yaw_gain_DAQ_AI ); 
-data.Inty = trial_data( :, settings.fictrac_y_DAQ_AI );
+data.ficTracIntx = trial_data( :, settings.fictrac_x_DAQ_AI ); %data from x channel
+data.ficTracAngularPosition = trial_data( :, settings.fictrac_yaw_gain_DAQ_AI ); 
+data.ficTracInty = trial_data( :, settings.fictrac_y_DAQ_AI );
 
 %Get filtered position and velocity data 
 smoothed = singleTrialVelocityAnalysis9mm(data, settings.sampRate);
