939e72dcbdbdc4bf61a7c297d207e3b22697f0b4
Tatsuo Okubo <tatsuo.okubo@gmail.com>
2021-03-03 15:20:34 -0500

    confirmed that plotting both works

--- matlab_code/analysis_utils/process_data_both.m
+++ matlab_code/analysis_utils/process_data_both.m
@@ -1,12 +1,16 @@
-%This code converts the panel and fictrac data to appropriate units and
-%calls on additional code to calculate velocities
+%%% converts fictrac data to appropriate units and calculate velocities
+%%% read panel and motor data and apply smoothing
+%%% based on process_data.m by Melanie Basnak
+%%% Tatsuo Okubo
+%%% 2021-03-03
 
-function [ t, stim_pos_vis, stim_pos_motor, vel_for, vel_yaw, fly_pos] = process_data_both( trial_time, trial_data, num_x_pixels)
+function [ t, stim_pos_panel, stim_pos_motor, vel_for, vel_yaw, fly_pos] = process_data_both( trial_time, trial_data, num_x_pixels)
 
-%import acquisition settings
+%% import acquisition settings
 settings = sensor_settings;
+sampRate_new = 25; % sampling rate after downsampling (Hz)
 
-%Asignment of the Daq channels
+%% Asignment of the DAQ channels
 settings.fictrac_x_DAQ_AI = 4;
 settings.fictrac_yaw_gain_DAQ_AI = 1;
 settings.fictrac_yaw_DAQ_AI = 3; 
@@ -20,22 +24,23 @@ data.Intx = trial_data( :, settings.fictrac_x_DAQ_AI ); %data from x channel
 data.angularPosition = trial_data( :, settings.fictrac_yaw_gain_DAQ_AI ); 
 data.Inty = trial_data( :, settings.fictrac_y_DAQ_AI );
 
-%Get filtered position and velocity data 
-smoothed = singleTrialVelocityAnalysis9mm(data, settings.sampRate);
+%% Get filtered position and velocity data 
+smoothed = singleTrialVelocityAnalysis9mm_TO(data, settings.sampRate, sampRate_new);
 vel_for = smoothed.xVel;
 vel_yaw = smoothed.angularVel;
 %vel_side = smoothed.yVel;
 
-%Get position and time data
+%% Get panel position and time data
 panels = trial_data( :, settings.panels_x_DAQ_AI ); %data from the x dimension in panels
 fly_pos = smoothed.degAngularPosition;
-[stim_pos_vis] = process_panel_360(panels, num_x_pixels); %returns filtered and downsampled panel px data as well as calculated angle of the bar
-[ t ] = resample_new(trial_time, 25, settings.sampRate); %downsamples the time
+[stim_pos_panel] = process_panel_360(panels, num_x_pixels); %returns filtered and downsampled panel px data as well as calculated angle of the bar
+[ t ] = resample_new(trial_time, sampRate_new, settings.sampRate); %downsamples the time
 
-downsampled.motor = resample_new(data.motor, sampRate_new, settings.sampRate);
+%% Get panel position
+motor = trial_data( :, settings.motor_DAQ_AI);
+downsampled.motor = resample_new(motor, sampRate_new, settings.sampRate);
 downsRad.motor = downsampled.motor .* 2 .* pi ./ 10; % from voltage to radian
 downsDeg.motor = downsRad.motor .* 360 ./ (2 * pi); 
-stim_pos = downsDeg.motor;
+stim_pos_motor = downsDeg.motor;
 
 end
-
---
+++ matlab_code/analysis_utils/singleTrialVelocityAnalysis9mm_TO.m
@@ -0,0 +1,144 @@
+function [smoothed] = singleTrialVelocityAnalysis9mm_TO(data, sampleRate, sampRate_new)
+% This function processes the raw position data obtained from FicTrac to
+% give back the velocity in degrees.
+
+% It takes the rawData (data) and the sample rate (sampleRate) as inputs
+% and gives back a struct names "smoothed" with the 3 final velocities for
+% x, y and angular velocity.  
+
+
+%% Tranform signal from voltage to radians for unwrapping
+
+    dataRad.Intx = data.Intx .* 2 .* pi ./ 10; %10 is for the max voltage outputed by the daq
+    dataRad.Inty = data.Inty .* 2 .* pi ./ 10;
+    dataRad.angularPosition = data.angularPosition .* 2 .* pi ./ 10;
+    
+
+% Now the position is going between 0 and 2 pi.
+
+%% Unwrapping 
+
+    unwrapped.Intx = unwrap(dataRad.Intx);
+    unwrapped.Inty = unwrap(dataRad.Inty);
+    unwrapped.angularPosition = unwrap(dataRad.angularPosition);
+
+% Now the position is unwrapped, so it doesn't jump when moving from 0 to
+% 2pi and vice versa
+
+
+%% Downsample the position data to match FicTrac's output
+
+
+    % Downsample to match FicTrac's output
+    downsampled.Intx = resample_new(unwrapped.Intx, sampRate_new, sampleRate); %For a 1000 rate acquisition frame rate from the NiDaq, downsampling to 25 Hz equals taking 1 every 40 frames
+    downsampled.Inty = resample_new(unwrapped.Inty, sampRate_new, sampleRate); %For a 1000 rate acquisition frame rate from the NiDaq, downsampling to 25 Hz equals taking 1 every 40 frames  
+    downsampled.angularPosition = resample_new(unwrapped.angularPosition, sampRate_new, sampleRate);
+    
+        
+% The output is downsampled. It isn't noticeable when plotting solid lines, and
+% it is barely noticeable when plotting dotted lines.
+%% Smooth the data
+
+    smoothed.Intx = smoothdata(downsampled.Intx, 'rlowess', sampRate_new); 
+    smoothed.Inty = smoothdata(downsampled.Inty, 'rlowess', sampRate_new); 
+    smoothed.angularPosition = smoothdata(downsampled.angularPosition, 'rlowess', sampRate_new);
+    
+     
+%% Transform to useful systems 
+    
+    deg.Intx = smoothed.Intx * 4.5; % wer tranform the pos to mm by scaling the value by the sphere's radius
+    deg.Inty = smoothed.Inty * 4.5;
+    deg.angularPosition = (smoothed.angularPosition / (2*pi)) * 360; % we transform the angular position to degrees
+    %subtract the initial angle from the set up (the positon 1 is actually
+    %3 pixels to the right of the fly)
+    initialPx = 3; %my x=1 positon is 3 pixels to the right of the animal
+    initialAngle = initialPx*360/96;
+    %shift according to the panels pos
+    shifted_data = deg.angularPosition - initialAngle;
+    %wrap
+    deg.angularPosition = wrapTo360(shifted_data);
+    smoothed.degAngularPosition = deg.angularPosition;
+     smoothed.angularPosition = deg2rad(deg.angularPosition);
+    
+    %add position for x and y in degrees to later compute total movement
+    deg.IntxDeg = rad2deg(smoothed.Intx);
+    deg.IntyDeg = rad2deg(smoothed.Inty);    
+    
+%% Take the derivative
+
+    diff.Intx = gradient(deg.Intx).* sampRate_new;
+    diff.Inty = gradient(deg.Inty).* sampRate_new; 
+    diff.angularPosition = gradient(deg.angularPosition).* sampRate_new;
+    
+    %%add the x and y in deg/s
+    diff.IntxDeg = gradient(deg.IntxDeg).* sampRate_new;
+    diff.IntyDeg = gradient(deg.IntyDeg).* sampRate_new;
+
+%% Calculate the distribution and take away values that are below 2.5% and above 97.5%
+    
+    percentile25AV = prctile(diff.angularPosition,2.5);
+    percentile975AV = prctile(diff.angularPosition,97.5);
+    boundedDiffAngularPos = diff.angularPosition;
+    boundedDiffAngularPos(diff.angularPosition<percentile25AV | diff.angularPosition>percentile975AV) = NaN;
+    
+    percentile25FV = prctile(diff.Intx,2.5);
+    percentile975FV = prctile(diff.Intx,97.5);
+    boundedDiffIntx = diff.Intx;
+    boundedDiffIntx(boundedDiffIntx<percentile25FV | boundedDiffIntx>percentile975FV) = NaN;
+    
+    percentile25SV = prctile(diff.Inty,2.5);
+    percentile975SV = prctile(diff.Inty,97.5);
+    boundedDiffInty = diff.Inty;
+    boundedDiffInty(boundedDiffInty<percentile25SV | boundedDiffInty>percentile975SV) = NaN;
+    
+    %add the deg/s components
+    percentile25FVDeg = prctile(diff.IntxDeg,2.5);
+    percentile975FVDeg = prctile(diff.IntxDeg,97.5);
+    boundedDiffIntxDeg = diff.IntxDeg;
+    boundedDiffIntxDeg(boundedDiffIntxDeg<percentile25FVDeg | boundedDiffIntxDeg>percentile975FVDeg) = NaN;
+    
+    percentile25SVDeg = prctile(diff.IntyDeg,2.5);
+    percentile975SVDeg = prctile(diff.IntyDeg,97.5);
+    boundedDiffIntyDeg = diff.IntyDeg;
+    boundedDiffIntyDeg(boundedDiffIntyDeg<percentile25SVDeg | boundedDiffIntyDeg>percentile975SVDeg) = NaN;
+
+ %% Linearly interpolate to replace the NaNs with values.
+ 
+    [pointsVectorAV] = find(~isnan(boundedDiffAngularPos));
+    valuesVectorAV = boundedDiffAngularPos(pointsVectorAV);
+    xiAV = 1:length(boundedDiffAngularPos);
+    interpAngVel = interp1(pointsVectorAV,valuesVectorAV,xiAV);
+    
+    [pointsVectorFV] = find(~isnan(boundedDiffIntx));
+    valuesVectorFV = boundedDiffIntx(pointsVectorFV);
+    xiFV = 1:length(boundedDiffIntx);
+    interpxVel = interp1(pointsVectorFV,valuesVectorFV,xiFV);
+    
+    [pointsVectorSV] = find(~isnan(boundedDiffInty));
+    valuesVectorSV = boundedDiffInty(pointsVectorSV);
+    xiSV = 1:length(boundedDiffInty);
+    interpyVel = interp1(pointsVectorSV,valuesVectorSV,xiSV);
+    
+    [pointsVectorFVDeg] = find(~isnan(boundedDiffIntxDeg));
+    valuesVectorFVDeg = boundedDiffIntxDeg(pointsVectorFVDeg);
+    xiFVDeg = 1:length(boundedDiffIntxDeg);
+    interpxVelDeg = interp1(pointsVectorFVDeg,valuesVectorFVDeg,xiFVDeg);
+    
+    [pointsVectorSVDeg] = find(~isnan(boundedDiffIntyDeg));
+    valuesVectorSVDeg = boundedDiffIntyDeg(pointsVectorSVDeg);
+    xiSVDeg = 1:length(boundedDiffIntyDeg);
+    interpyVelDeg = interp1(pointsVectorSVDeg,valuesVectorSVDeg,xiSVDeg);
+       
+ %%  Smooth again
+ 
+    smoothed.xVel = smoothdata(interpxVel,'rlowess',15);
+    smoothed.yVel = smoothdata(interpyVel,'rlowess',15);
+    smoothed.angularVel = smoothdata(interpAngVel,'rlowess',15);
+    % add the deg/s components
+    smoothed.xVelDeg = smoothdata(interpxVelDeg,'rlowess',15);
+    smoothed.yVelDeg = smoothdata(interpyVelDeg,'rlowess',15);
+    %add total movement
+    smoothed.total_mvt = abs(smoothed.angularVel)+abs(smoothed.xVelDeg)+abs(smoothed.yVelDeg);
+
+
+end
