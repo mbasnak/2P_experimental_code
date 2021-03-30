df85185b8e670c9d429d5a8e263868256f935b3c
Melanie Basnak <melaniebasnak@g.harvard.edu>
2021-03-30 16:16:30 -0400

    Update begin_trials.m
    added appropriate files to run and display wind trials

--- matlab_code/begin_trials.m
+++ matlab_code/begin_trials.m
@@ -54,19 +54,12 @@ for i = 1:task_cnt
         [trial_bdata, trial_time] = run_panels_trial(i, cur_task, run_obj, scanimage_client_skt, cur_trial_corename );
         [fwd_histogram, ang_histogram, fly_pos_histogram] = display_trial(session_id, i-1, run_obj, trial_time, trial_bdata, session_fig,fwd_histogram, ang_histogram, fly_pos_histogram);
         
-    %If only running wind (this need to be changed)
-    elseif (strcmp(run_obj.panel_status, 'On') == 0) & (strcmp(run_obj.wind_status, 'On') == 1)        
+    %If only running wind
+    elseif (strcmp(run_obj.panel_status, 'On') == 0) && (strcmp(run_obj.wind_status, 'On') == 1)        
         cur_task = run_obj.wind_mode;
         cur_trial_corename = ['wind_' cur_task '_' datestr(now, 'yyyymmdd_HHMMSS') '_sid_' num2str(session_id) '_tid_' num2str(i-1)];
-        TrialType = [1 2 2 1 1 2 1 2 2 2 2 2 1 1 2 1 1 1 2 1 1 2 2 1 2 1 1 1 2 1 2 1 1 1 2 2 2 2 2 1];
-        switch TrialType(i)
-            case 1 % L->R
-                cur_task = 'WindLeftRight';
-            case 2 % R->L
-                cur_task = 'WindRightLeft';
-        end
-        cur_trial_corename = [cur_task '_' datestr(now, 'yyyymmdd_HHMMSS') '_sid_' num2str(session_id) '_tid_' num2str(i-1)];
-        %[trial_bdata, trial_time] = run_wind_trial(i, cur_task, run_obj, scanimage_client_skt, cur_trial_corename );
+        [trial_bdata, trial_time] = run_wind_trial(i, cur_task, run_obj, scanimage_client_skt, cur_trial_corename );
+        [fwd_histogram, ang_histogram, fly_pos_histogram] = display_trial_both(session_id, i-1, run_obj, trial_time, trial_bdata, session_fig,fwd_histogram, ang_histogram, fly_pos_histogram);
         
     %If running both
     elseif (strcmp(run_obj.panel_status, 'On') == 1) & (strcmp(run_obj.wind_status, 'On') == 1)  
