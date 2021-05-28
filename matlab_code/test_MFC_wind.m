87b75ae5e43a1bc8e0431f34c7c12b783a5d2129
Tatsuo Okubo <tatsuo.okubo@gmail.com>
2021-05-28 15:32:29 -0400

    created small code to test MFC function

---
+++ matlab_code/test_MFC_wind.m
@@ -0,0 +1,42 @@
+%%% function test_MFC_wind(PulseDur)
+%%% test the pinch valve used for closed-loop wind device
+%%% INPUTS:
+%%%     pulse_dur: duration of the pulse in sec
+%%% OUTPUTS:
+%%%     None
+%%%
+%%% Tatsuo Okubo
+%%% 2021-05-28
+
+%% parameters
+pulse_dur = 10; %[s]
+flow_rate = 0.1; % [L/min]
+
+%% setup DAQ
+set_up = '2P-room';
+s = setup_nidaq(set_up);
+settings = nidaq_settings;
+SAMPLING_RATE = settings.sampRate;
+s.Rate = SAMPLING_RATE; %sampling rate for the session (Jenny is using 4000 Hz)
+
+%% preparation
+MFC = MFC_settings;
+MFC_trigger = (flow_rate / MFC.MAX_FLOW) * MFC.MAX_V * ones(SAMPLING_RATE * MFC.WAIT, 1); %convert the airflow signal to voltage
+imaging_trigger = zeros(SAMPLING_RATE * MFC.WAIT, 1);
+valve_trigger = zeros(SAMPLING_RATE * MFC.WAIT, 1);
+output_data = [MFC_trigger, imaging_trigger, valve_trigger];
+queueOutputData(s, output_data);
+s.startForeground();
+
+%% reak pulse
+MFC_trigger = (flow_rate / MFC.MAX_FLOW) * MFC.MAX_V * ones(SAMPLING_RATE * pulse_dur, 1); %convert the airflow signal to voltage
+imaging_trigger = zeros(SAMPLING_RATE * pulse_dur, 1);
+valve_trigger = ones(SAMPLING_RATE * pulse_dur, 1);
+valve_trigger(1) = 0;
+valve_trigger(end) = 0;
+output_data = [MFC_trigger, imaging_trigger, valve_trigger];
+queueOutputData(s, output_data);
+[output,time] = s.startForeground();
+
+figure,
+plot(time,output(:,9))
