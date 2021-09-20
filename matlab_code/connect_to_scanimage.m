d8e68f3d7bd0183aecba2d11ef327ec59e7bfc40
Tatsuo Okubo <tatsuo.okubo@gmail.com>
2021-09-20 10:37:24 -0400

    update IP address of 2p computer

--- matlab_code/connect_to_scanimage.m
+++ matlab_code/connect_to_scanimage.m
@@ -2,7 +2,7 @@ function [ skt ] = connect_to_scanimage()
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-skt = tcpip('10.119.97.143', 30000, 'NetworkRole', 'client');
+skt = tcpip('10.119.97.242', 30000, 'NetworkRole', 'client'); %if this code is giving an error, change the first argument by whatever appears as 'ipv4 address' in crane using 'ipconfig /all'
 fopen(skt);
 flushinput(skt);
 
