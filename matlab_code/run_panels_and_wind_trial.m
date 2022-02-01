7077a850be5cd1e3da036f8350b7762034585c00
Tatsuo Okubo <tatsuo.okubo@gmail.com>
2022-02-01 13:41:03 -0500

    added CL wind + OL panels for static wind (gain=0)

--- matlab_code/run_panels_and_wind_trial.m
+++ matlab_code/run_panels_and_wind_trial.m
@@ -62,6 +62,8 @@ elseif strcmp(task, 'panels_Closed_Loop_wind_Open_Loop') == 1
     closedLoop(run_obj.pattern_number, start_x, start_y);
 elseif strcmp(task, 'panels_Open_Loop_wind_Open_Loop') == 1
     closedLoop(run_obj.pattern_number, start_x, start_y);  % use closedLoop so that panel is synced with motor
+elseif strcmp(task, 'panels_Open_Loop_wind_Closed_Loop') == 1 % useful to have static wind (gain=0) and open-loop bar
+    openLoop(run_obj.pattern_number, run_obj.function_number);
 else
     error('task not implemented')
 end
@@ -72,7 +74,7 @@ Panel_com('start');
 %% start the trial
 %Run the python script that runs fictrac and other experimental conditions
 if (strcmp(run_obj.experiment_type,'Spontaneous_walking')==1)
-    if (strcmp(task, 'panels_Closed_Loop_wind_Closed_Loop') == 1 | strcmp(task, 'panels_Closed_Loop_X_Open_Loop_Y_wind_Closed_Loop') == 1)
+    if (strcmp(task, 'panels_Closed_Loop_wind_Closed_Loop') == 1 | strcmp(task, 'panels_Closed_Loop_X_Open_Loop_Y_wind_Closed_Loop') == 1 | strcmp(task, 'panels_Open_Loop_wind_Closed_Loop') == 1)
         system(['python.exe run_socket_client_wind.py ' num2str(run_obj.experiment_type) ' ' num2str(run_obj.trial_t) ' "' hdf_file '" ' num2str(run_obj.start_pos_x) ' ' num2str(run_obj.gain_panels) ' ' num2str(run_obj.gain_wind) ' 1 &']);
     elseif strcmp(task, 'panels_Closed_Loop_wind_Open_Loop') == 1
         if run_obj.modulated_speed.Value == 1
